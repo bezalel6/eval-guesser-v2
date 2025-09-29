d7e9a2737d42426c7b3b3aa7c4cce7d942eff4e2
bezalel6 <bezalel3250@gmail.com>
2025-09-29 13:06:23 +0300

    fluff cleanup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,7 +17,9 @@
 	function updateTheme() {
 		if (theme === 'dark') {
 			document.documentElement.classList.add('dark');
+			document.documentElement.classList.remove('light');
 		} else {
+			document.documentElement.classList.add('light');
 			document.documentElement.classList.remove('dark');
 		}
 	}
